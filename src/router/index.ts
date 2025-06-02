52eb8123ec60d30d574e00bd11282fd7ba84d4f9
supastishn <omareeto2012@hotmail.com>
2025-06-02 18:24:04 +0000

    refactor: Use relative paths for router imports

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '@/App.vue'
-import Login from '@/components/Login.vue'
-import Register from '@/components/Register.vue'
-import HistoryPage from '@/components/HistoryPage.vue'
+import Home from '../App.vue'  // Changed from '@/App.vue'
+import Login from '../components/Login.vue'  // Changed from '@/components/Login.vue'
+import Register from '../components/Register.vue'  // Changed from '@/components/Register.vue'
+import HistoryPage from '../components/HistoryPage.vue'  // Changed from '@/components/HistoryPage.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
