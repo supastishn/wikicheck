598308bbf663891a061f6614b05bcaeab338ff03
supastishn <omareeto2012@hotmail.com>
2025-06-03 18:12:36 +0000

    fix: Revert HTTP method to POST with ts-ignore

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -20,7 +20,8 @@ const checkStatement = async () => {
       JSON.stringify({ statement: statement.value }),
       false,
       '/',
-      'post',
+      // @ts-ignore
+      'POST',
       {'Content-Type': 'application/json'}
     )
 
